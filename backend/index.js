d42f8acdba1f2272072b7f2f29e94c6991668240
Sandesh-7331 <patilsandesh90058@gmail.com>
2026-06-14 17:04:59 +0530

    Add product upload and addproduct route

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.get("/",(req,res)=>{
 const storage = multer.diskStorage({
     destination : './uploads/images',
     filename : (req,file,cb)=>{
-        return cb(null,'${file.fieldname}_${Date.now()}${path.extname(file.originalname)}')
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 
@@ -34,7 +34,61 @@ app.use('/images',express.static('uploads/images'))
 app.post("/upload",upload.single('product'),(req,res)=>{ 
     res.json({
         success : 1,
-        image_url : 'http://localhost:${port}/images/${req.file.filename}'
+        image_url : `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for Creating Products
+const product = mongoose.model("product",{
+    id: {
+        type : Number,
+        required : true,
+    },
+    name: {
+        type : String,
+        required : true,
+    },
+    image: {
+        type : String,
+        required : true,
+    },
+    category: {
+        type : String,
+        required : true,
+    },
+    new_price: {
+        type : Number,
+        required : true,
+    },
+    old_price: {
+        type : Number,
+        required : true,
+    },
+    date : {
+        type : Date,
+        default : Date.now,
+    },
+    available : {
+        type : Boolean,
+        default : true,
+    },
+})
+
+app.post('/addproduct',async (req,res)=>{
+    const newProduct = new product({ 
+        id : req.body.id,
+        name : req.body.name,
+        image : req.body.image,
+        category : req.body.category,
+        new_price : req.body.new_price,
+        old_price : req.body.old_price,
+    });
+    console.log(newProduct);
+    await newProduct.save();
+    console.log("Saved");
+    res.json({
+        success : true,
+        name :req.body.name
     })
 })
 
